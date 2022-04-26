69ce969d4f0619bee23875dd0621fc7339250d88
Vladimir Pisacic <vladimir.pisacic@qualityhouse.rs>
2022-04-26 15:29:18 +0200

    contact step definition unique

--- src/test/java/stepDefinitions/Contact.java
+++ src/test/java/stepDefinitions/Contact.java
@@ -6,9 +6,12 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Contact {
     private WebDriver driver;
@@ -26,38 +29,43 @@ public class Contact {
     }
 
 
+
+    public String generateRandomNumber(int length){
+        return RandomStringUtils.randomNumeric(length);
+    }
+    public String generateRandomString(int length){
+        return RandomStringUtils.randomAlphabetic(length);
+    }
+
     @Given("I access the web driver university contact us page")
     public void i_access_the_web_driver_university_contact_us_page() {
      driver.get("https://webdriveruniversity.com/Contact-Us/contactus.html");
     }
     @When("I enter a unique first name")
     public void i_enter_a_unique_first_name() {
-       driver.findElement(By.xpath(""))
+       driver.findElement(By.xpath("//*[@id=\"contact_form\"]/input[1]")).sendKeys("FirstName"+generateRandomNumber(5));
     }
     @When("I enter a unique last name")
     public void i_enter_a_unique_last_name() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+     driver.findElement(By.xpath("//*[@id=\"contact_form\"]/input[2]")).sendKeys("LastName"+generateRandomNumber(6));
     }
     @When("I enter a unique email address")
     public void i_enter_a_unique_email_address() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    driver.findElement(By.xpath("//*[@id=\"contact_form\"]/input[3]")).sendKeys("Email"+generateRandomNumber(10)+"gmail.com");
     }
     @When("I enter a unique comment")
     public void i_enter_a_unique_comment() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+ driver.findElement(By.xpath("//*[@id=\"contact_form\"]/textarea")).sendKeys("Hello World"+ generateRandomString(20));
     }
     @When("I click on the submit button")
     public void i_click_on_the_submit_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+    driver.findElement(By.xpath("//*[@id=\"form_buttons\"]/input[2]")).click();
     }
     @Then("I should be presented wit a successful contact us submission message")
     public void i_should_be_presented_wit_a_successful_contact_us_submission_message() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+       WebElement contactUs_Submission_Message = driver.findElement(By.xpath("//*[@id=\"contact_reply\"]/h1"));
+       Assert.assertEquals(contactUs_Submission_Message.getText(),"Thank You for your Message!");
+
     }
 
 
